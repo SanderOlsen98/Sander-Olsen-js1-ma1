2a7407d3fe841026b586f6c5dbda8f368fd9aa79
Gamborg Madeleine <Madeleine.Gamborg@posten.no>
2021-06-14 20:56:04 +0200

    update

--- js/script.js
+++ js/script.js
@@ -12,3 +12,29 @@ const cats = [
     }
 ];
 
+
+// Question 1
+// Create an object called cat
+
+const animal = {
+    type: "cat",
+};
+
+console.log(animal.type)
+
+if(animal.type){
+    console.log("Meow!")
+}
+
+
+// Question 2
+// Update heading 
+
+const heading = document.querySelector("h3");
+
+heading.innerHTML = `
+    <h3 style="font-size: 2em">Updated Heading </h3>
+`
+
+// Question 3
+// change style
